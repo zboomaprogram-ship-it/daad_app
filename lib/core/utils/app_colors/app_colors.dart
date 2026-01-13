7b8e3376530f241b99c8fb1e0acb85444a1b78bc
zboomaprogram-ship-it <zbooma.program@gmail.com>
2026-01-13 15:59:27 +0200

    remove recording

--- lib/core/utils/app_colors/app_colors.dart
+++ lib/core/utils/app_colors/app_colors.dart
@@ -4,12 +4,8 @@ class AppColors {
   // ✅ ثابت دائماً
   // static const Color primaryColor = Color(0xff5f1c32);
   static const Color primaryColor = Color(0xff320D19);
-  static const Color secondaryTextColor= Color(0xffDFD8BC);
+  static const Color secondaryTextColor = Color(0xffDFD8BC);
   static const Color textColor = Color.fromARGB(255, 255, 255, 255);
-    static const Color backgroundColor = Color(0xff5f1c32);
-      static const Color secondaryColor = Colors.transparent;
-
-
-
-
+  static const Color backgroundColor = Color(0xff5f1c32);
+  static const Color secondaryColor = Colors.transparent;
 }
